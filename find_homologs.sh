8e995d0f4fc1fb98b7eac02fae7575ad51f05ebb
Anagha <anaghakrishna92@gmail.com>
2023-09-09 23:29:50 -0400

    Working version

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,9 +6,7 @@
 # Use awk for column-based filtering to extract rows which match 100%
 # and all 28 characters in CRISPR sequence and redirect to output file 
 tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' | 
-awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0 $4/$13;}' > $3
-
-head -10 $3
+awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0;}' > $3
 
 # Count the number of lines in output file to obtain number of perfect matches
-#wc -l $3 
+wc -l $3 
