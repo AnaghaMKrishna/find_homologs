ac1060f318ae035721a783ee8bbb199ccea05e33
Anagha <anaghakrishna92@gmail.com>
2023-09-09 23:37:17 -0400

    Updated comments

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,12 +1,12 @@
 #! /usr/bin/bash
-# usage: ./find_perfect_matches.sh <query file> <subject file> <output file>
+# usage: ./find_homologs.sh <query file> <subject file> <output file>
 
-# BLAST command matches the CRISPR sequence with assemblies in 
-# subject files and  outputs the standard 12 columns and aligned subsequence.
-# Use awk for column-based filtering to extract rows which match 100%
-# and all 28 characters in CRISPR sequence and redirect to output file 
+# tblastn command matches the query protein sequence with genome sequence in 
+# subject files and outputs the standard 12 columns and query length.
+# Use awk for column-based filtering to extract rows which match more than 
+# 30% and have more than 90% match length and redirect to output file 
 tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' | 
 awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0;}' > $3
 
-# Count the number of lines in output file to obtain number of perfect matches
+# Count the number of lines in output file to obtain number of matches
 wc -l $3 
