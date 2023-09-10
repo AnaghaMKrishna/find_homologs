5ef2f2275919a6fad4acac0f7c6fd6756907a82b
Anagha <anaghakrishna92@gmail.com>
2023-09-09 23:01:02 -0400

    Filtering using awk

--- find_homologs.sh
+++ find_homologs.sh
@@ -5,10 +5,10 @@
 # subject files and  outputs the standard 12 columns and aligned subsequence.
 # Use awk for column-based filtering to extract rows which match 100%
 # and all 28 characters in CRISPR sequence and redirect to output file 
-tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' > $3
-head $3
+tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' | 
+awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0 $4/$13;}' > $3
 
-#awk '{if ($3 > 30 && $4 == 28) print $0;}' > $3
+head -10 $3
 
 # Count the number of lines in output file to obtain number of perfect matches
 #wc -l $3 
