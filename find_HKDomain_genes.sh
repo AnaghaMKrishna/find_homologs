a32a5a452361a7d9430fd38895e3ce16c68ec78b
Anagha <anaghakrishna92@gmail.com>
2023-09-16 15:19:03 -0400

    Reading input files and extracting sstart and send from tblastn output

--- find_HKDomain_genes.sh
+++ find_HKDomain_genes.sh
@@ -1,12 +1,26 @@
 #! /usr/bin/bash
-# usage: ./find_homologs.sh <query file> <subject file> <output file>
+# usage: ./find_homologs.sh <query.faa> <subject.fna> <bedfile> <output file>
 
+# Read args
+HKDomain_query_file=$1
+subject_file=$2
+bed_file=$3
+HKDomain_genes_file=$4
+
+#Create temp files for storing intermediate outputs
+tblastn_output_temp=$(mktemp)
 # tblastn command matches the query protein sequence with genome sequence in 
 # subject files and outputs the standard 12 columns and query length.
 # Use awk for column-based filtering to extract rows which match more than 
 # 30% and have more than 90% match length and redirect to output file 
-tblastn -query $1 -subject $2 -task tblastn -outfmt '6 std qlen' | 
-awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0;}' > $3
+tblastn \
+    -query $HKDomain_query_file \
+    -subject $subject_file \
+    -task tblastn \
+    -outfmt '6 std qlen' \
+| awk '{if ($3 > 30.000 && $4 > 0.9*$13 ) print $0;}' > $tblastn_output_temp
+
+cut -f 1,9,10 $tblastn_output_temp
 
 # Count the number of lines in output file to obtain number of matches
-wc -l $3 
+cat $tblastn_output_temp
